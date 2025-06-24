b6d69330041d818a4d6b0498225feb11ecc58289
mypc <lsrmod2014@163.com>
2025-06-25 00:19:51 +0800

    update datas

--- main.py
+++ main.py
@@ -272,7 +272,7 @@ def whimsical_fupan_analyze():
 
 def generate_ladder_chart():
     start_date = '20250401'  # 调整为Excel中有数据的日期范围
-    end_date = '20250623'  # 过了0点最好指定日期
+    end_date = '20250624'  # 过了0点需指定日期
     min_board_level = 3
     non_main_board_level = 2
     show_period_change = True  # 是否计算周期涨跌幅
@@ -284,7 +284,7 @@ def generate_ladder_chart():
     ]
 
     # 构建梯队图
-    build_ladder_chart(start_date, None, min_board_level=min_board_level,
+    build_ladder_chart(start_date, end_date, min_board_level=min_board_level,
                        non_main_board_level=non_main_board_level, show_period_change=show_period_change,
                        priority_reasons=priority_reasons, enable_attention_criteria=True,
                        sheet_name=sheet_name)
