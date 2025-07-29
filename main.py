af39772b8415edfc67f971e7d0a5f2f083ea76ac
mypc <lsrmod2014@163.com>
2025-07-30 00:13:51 +0800

    update datas

--- main.py
+++ main.py
@@ -301,7 +301,7 @@ def update_synonym_groups():
 def whimsical_fupan_analyze():
     # 执行归类分析
     start_date = "20250630"
-    end_date = "20250728"
+    end_date = None
 
     process_zt_data(start_date, end_date, clean_output=True)
     # add_vba_for_excel()
@@ -312,7 +312,7 @@ def whimsical_fupan_analyze():
 
 def generate_ladder_chart():
     start_date = '20250501'  # 调整为Excel中有数据的日期范围
-    end_date = '20250728'  # 过了0点需指定日期
+    end_date = None  # 过了0点需指定日期
     min_board_level = 2
     non_main_board_level = 2
     show_period_change = True  # 是否计算周期涨跌幅
