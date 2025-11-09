b07c38d7401aa250b2079d9e4b8c96ee5ca67a9d
mypc <lsrmod2014@163.com>
2025-11-09 23:58:56 +0800

    update datas

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,5 @@
 # 保存为check_numpy.py并运行
-import sys
-import talib
+import akshare as ak
 
-print(f"talib版本: {talib.__version__}")
+all_data = ak.stock_zh_index_daily(symbol='sz399006')
+print(all_data.tail(10))
