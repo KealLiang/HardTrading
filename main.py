22ff645dfa9159777ea205ffcaa5d4ef6cba026f
mypc <lsrmod2014@163.com>
2026-04-15 18:53:53 +0800

    update datas

--- main.py
+++ main.py
@@ -1702,11 +1702,11 @@ def vcp_score_analysis(scan_file: str, backtest_file: str):
 if __name__ == '__main__':
     # === 热门天梯 ===
     # whimsical_fupan_analyze()
-    generate_ladder_chart()
+    # generate_ladder_chart()
     # generate_leader_sheet_html_charts(columns=2, before_days=60, after_days=30)  # 虚拟K线 virtual_bars=[(-3.0, 10.0), (5, -5)]
 
     # === 复盘相关 ===
-    # daily_routine()
+    daily_routine()
     # get_stock_datas()
     # full_scan_routine()
     # get_index_data()
